7442b054fd75d3a0a8ac8ea490e8c6cc6fe801cf
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-08-03 09:01:18 +0530

    add User class

---
+++ src/User.java
@@ -0,0 +1,20 @@
+public class User {
+    private final String username;
+    private final String password;
+    private final String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+
+    public static User fromFileString(String line) {
+        String[] parts = line.split(",");
+        return new User(parts[0], parts[1], parts[2]);
+    }
+}
